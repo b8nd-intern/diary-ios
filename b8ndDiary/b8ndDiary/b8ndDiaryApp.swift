e70549d86ab1707d50dead76d0c596792b0cb16c
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-09-21 20:03:17 +0900

    init project

---
+++ b8ndDiary/b8ndDiary/b8ndDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  b8ndDiaryApp.swift
+//  b8ndDiary
+//
+//  Created by dgsw8th71 on 2023/09/21.
+//
+
+import SwiftUI
+
+@main
+struct b8ndDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
