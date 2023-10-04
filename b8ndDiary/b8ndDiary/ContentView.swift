d8dbac153de7cd273af3f18347e44aace2a68e17
bestswlkh0310 <hhhello0507@gmail.com>
2023-10-04 21:44:08 +0900

    add file

---
+++ b8ndDiary/b8ndDiary/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  b8ndDiary
+//
+//  Created by dgsw8th71 on 2023/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {}
+    }
+} 
---
+++ b8ndDiary/b8ndDiary/View/Home/HomeView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView.swift
+//  b8ndDiary
+//
+//  Created by dgsw8th71 on 2023/10/04.
+//
+
+import Foundation
