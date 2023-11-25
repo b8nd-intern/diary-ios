32527ac91f33183fc111d43d78ee4a61ef8d5ab9
leeyoonchae <130755399+leeyoonchae@users.noreply.github.com>
2023-11-25 14:41:00 +0900

    add : postMonth

--- b8ndDiary/b8ndDiary/View/Profile/MonthPage.swift
+++ b8ndDiary/b8ndDiary/View/Profile/MonthPage.swift
@@ -6,14 +6,39 @@
 //
 import SwiftUI
 import GoogleSignIn
+import Network
+//class MyMonthPost: ObservableObject {
+////    @Published var dataModels: [DataModel] = [] // 관찰 가능한 속성 추가
+//    
+//    static func getMonthPost() async throws -> [DataModel] {
+//    
+//        let params = ["year": 2023,"month": 1 ]
+//        
+//        let response = try await HttpClient.request(HttpRequest(url: "post/month", method: .get, params :params, model: Response<[DataModel]>.self))
+//        
+//        return response.data ?? [] // response.data가 nil이면 빈 배열 반환
+//    }
+//}
+class MonthPost: ObservableObject {
+    @Published var dataModels: [DataModel] = []
+
+    static func postMonth(month: Int) async throws -> Response<[DataModel]> { // month 매개변수 추가
+        let year = 2023
+        let response = try await HttpClient.request(HttpRequest(url: "post/month/\(year)/\(month)", method: .get, model: Response<[DataModel]>.self))
+        
+        print(response)
+        
+        return response
+    }
+}
 
 
 struct MonthPage: View {
-    var selectedMonth: String
+    var selectedMonth: Int
     
     @Environment(\.presentationMode) var presentationMode
     
-
+    @StateObject private var myMonthPost = MonthPost()
     
     var body: some View {
         VStack {
@@ -39,6 +64,17 @@ struct MonthPage: View {
                     Spacer()
                 }
         )
-
+        .onAppear {
+            Task {
+                do {
+                    let response = try await MonthPost.postMonth(month: selectedMonth) // selectedMonth 값을 전달
+                    DispatchQueue.main.async {
+                        myMonthPost.dataModels = response.data ?? []
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+        }
     }
 }
