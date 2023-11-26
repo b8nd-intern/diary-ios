db02993a8413e0784bcccf54b776cef581e6d342
leeyoonchae <130755399+leeyoonchae@users.noreply.github.com>
2023-11-26 15:59:46 +0900

    fix : monthpage

--- b8ndDiary/b8ndDiary/Data/Response/Monthpost.swift
+++ b8ndDiary/b8ndDiary/Data/Response/Monthpost.swift
@@ -9,6 +9,24 @@ import Foundation
 
 class MonthPost: ObservableObject {
     @Published var dataModels: [DataModel] = []
+    
+    @Published var offset: CGFloat = 0
+    //    @Published var direct: Direct = .none
+    private var originOffset: CGFloat = 0
+    private var isCheckedOriginOffset: Bool = false
+    
+    func setOriginOffset(_ offset: CGFloat) {
+        guard !isCheckedOriginOffset else { return }
+        self.originOffset = offset
+        self.offset = offset
+        isCheckedOriginOffset = true
+    }
+    
+    func setOffset(_ offset: CGFloat) {
+        guard isCheckedOriginOffset else { return }
+        self.offset = offset
+    }
+    
 
     static func postMonth(month: Int) async throws -> Response<[DataModel]> { // month 매개변수 추가
         let year = 2023
--- b8ndDiary/b8ndDiary/View/Profile/MonthPage.swift
+++ b8ndDiary/b8ndDiary/View/Profile/MonthPage.swift
@@ -1,27 +1,7 @@
-//
-//  MonthPage.swift
-//  GoogleLogin
-//
-//  Created by dgsw8th61 on 2023/10/06.
-//
 import SwiftUI
 import Network
 
-struct ScrollOffsetKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
-    
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = nextValue()
-    }
-}
-
 struct MonthPage: View {
-    struct ScrollOffsetKey: PreferenceKey {
-        static var defaultValue: CGFloat = .zero
-        static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-            value += nextValue()
-        }
-    }
     
     var selectedMonth: Int
     @State var isClicked: Bool = false
@@ -32,22 +12,22 @@ struct MonthPage: View {
     
     @StateObject private var myMonthPost = MonthPost()
     
+    
+    private var scrollObservableView: some View {
+        GeometryReader { proxy in
+            let offsetY = proxy.frame(in: .global).origin.y
+            Color.clear
+                .preference(
+                    key: ScrollOffsetKey.self,
+                    value: offsetY
+                )
+                .onAppear {
+                    myMonthPost.setOriginOffset(offsetY)
+                }
+        }
+        .frame(height: 0)
+    }
     var body: some View {
-        let scrollObservableView: some View = {
-            GeometryReader { proxy in
-                let offsetY = proxy.frame(in: .global).origin.y
-                Color.clear
-                    .preference(
-                        key: ScrollOffsetKey.self,
-                        value: offsetY
-                    )
-                    .onAppear {
-                        myMonthPost.setOriginOffset(offsetY)
-                    }
-            }
-            .frame(height: 0)
-        }()
-        
         VStack {
             Text("\(selectedMonth)월")
                 .foregroundColor(.black)
@@ -55,6 +35,18 @@ struct MonthPage: View {
                 .bold()
                 .padding(.top, 20)
                 .padding(.trailing, 240)
+            
+            
+            ZStack{
+                //이미지 불러오는 부분
+                ShowDiaryView(isClicked: $isClicked, clickedContent: $clickedContent, day: $day, diaryContent: myMonthPost.dataModels)
+                    .padding(.horizontal, 30)
+            }
+            .padding(.top, 20)
+            .onPreferenceChange(ScrollOffsetKey.self) {
+                myMonthPost.setOffset($0)
+            }
+            
             Spacer()
         }
         .navigationBarBackButtonHidden()
@@ -68,16 +60,9 @@ struct MonthPage: View {
                             .foregroundColor(.black)
                             .cornerRadius(10)
                     })
-                    Spacer()
-                    ZStack{
-                        //이미지 불러오는 부분
-                        ShowDiaryView(isClicked: $isClicked, clickedContent: $clickedContent, day: $day, diaryContent: myMonthPost.dataModels)
-                            .padding(.horizontal, 40)
-                    }
-                    .padding(.top, 30)
-                    .onPreferenceChange(ScrollOffsetKey.self) {
-                        myMonthPost.setOffset($0)
-                    }
+      
+                    
+                    
                 }
         )
         .onAppear {
@@ -86,13 +71,19 @@ struct MonthPage: View {
                     let response = try await MonthPost.postMonth(month: selectedMonth) // selectedMonth 값을 전달
                     DispatchQueue.main.async {
                         myMonthPost.dataModels = response.data ?? []
-                        myMonthPost.setOffset(myMonthPost.offset)
                     }
                 } catch {
                     print(error)
                 }
             }
         }
-        .background(scrollObservableView)
+//        .background(scrollObservableView)
+    }
+    struct ScrollOffsetKey: PreferenceKey {
+        static var defaultValue: CGFloat = 0
+        
+        static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+            value += nextValue()
+        }
     }
 }
